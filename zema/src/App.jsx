e1f2391f67923d0271b9b26433c2fd0adeffa6f9
Mignot Mesele <146313078+SirCartier50@users.noreply.github.com>
2025-09-27 21:59:59 -0700

    Add files via upload

--- zema/src/App.jsx
+++ zema/src/App.jsx
@@ -1,18 +1,48 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import Scanner from './pages/scanner'
+import ProductScanner from './pages/product_scan'
+import { Route, Routes } from "react-router-dom"
+import Dashboard from './pages/dashboard'
+import Welcome from './pages/welcome'
+import { Navigate } from 'react-router-dom'
+import About from './pages/about_us'
+import Login from './pages/login'
+import { auth} from './utils/firebase';
+import Signup from './pages/signup'
+import Layout from './layout'
 function App() {
-  const [count, setCount] = useState(0)
+  const [user, setUser] = useState(null)
+  const [loading, setLoading] = useState(true);
 
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      setUser(user);
+      setLoading(false);
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
   return (
     <>
-      <div>
-        <div>
-        
-        </div>
-      </div>
+      <Routes>
+        <Route element={<Layout/>}>
+          <Route path="/" element={user ? <Dashboard /> : <Welcome />} />
+          <Route path="/scanner" element={user ? <Scanner/> : <Navigate to="/" />}/>
+          <Route path="/dashboard" element={user ? <Dashboard /> : <Navigate to="/" />} />
+          <Route path="/product_scan" element={user ? <ProductScanner/> : <Navigate to="/" />}/>
+          <Route path="/welcome" element={<Navigate to="/" />} />
+          <Route path="/about_us" element={<About/>} />
+          <Route path="/login" element={<Login/>} />
+          <Route path="/signup" element={<Signup/>} />
+        </Route>
+      </Routes>
     </>
   )
 }
---
+++ zema/src/layout.jsx
@@ -0,0 +1,18 @@
+import { Outlet } from "react-router-dom";
+import Navbar from "./components/navbar";
+
+const Layout = () => {
+    return (
+        <div>
+
+            <Navbar />
+            
+
+            <div className="pt-20">
+                <Outlet />
+            </div>
+        </div>
+    );
+};
+
+export default Layout;
